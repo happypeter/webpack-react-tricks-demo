e99180e7bc8935469dd70fb6ddef92e2790c28cf
Peter Wang <happypeter1983@gmail.com>
2017-01-12 10:03:13 +0800

    ep6 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import  { Component } from 'react';
+import  React, { Component } from 'react';
 
 
 class App extends Component {
   render(){
     return(
       <div>
-        App
+        App Dev
       </div>
     )
   }
